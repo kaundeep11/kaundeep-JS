f1da4a8e84d6de315992214c9ddd0c151e9dd929
kaundeep11 <deepraj692270@gmail.com>
2025-10-25 09:47:50 +0530

    02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello World!"); we are using node js not browser
+
+console.log(3 
+    + 
+    4); //code readability is important.
+
+console.log("Hello World!");
+
+
+let name = "John Doe";
+let age = 30;
+let isLoggedIn = true;
+let state; //undefined
+
+//primitives data types
+//number => 2 to power 53
+// BigInt => 2 to power 53 + 1
+// string => "John Doe"
+// boolean => true/false
+// null => empty value(standalone value)
+// undefined => variable is declared but not assigned a value
+// symbol => unique and immutable value
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
